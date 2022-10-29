0413056188171a91a4f16a11b6ab7e79b0d25001
NancyChT <106347956+NancyChT@users.noreply.github.com>
2022-10-29 10:26:53 -0600

    Creacion de base de datos

---
+++ Vista/Datos/CadenaConexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class CadenaConexion
+    {
+        public static string Cadena = "Data Source=localhost; Initial Catalog=UsuarioInicioSesion; User ID=root; Password=HERRAMIENTAnk1*";
+    }
+}
